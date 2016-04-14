c699042a4ccd669df7e43de11d344327e866200f
Christine Movius <christine.movius@gmail.com>
2016-04-14 10:45:45 -0400

    Add compliment and praise schema.

---
+++ db/connection.js
@@ -0,0 +1,25 @@
+var mongoose  = require("mongoose");
+
+var PraiseSchema = new mongoose.Schema(
+  {
+    praise: String
+  }
+);
+
+var ComplimentSchema = new mongoose.Schema(
+  {
+    compliment: String,
+    photo_url: String,
+    comments: [PraiseSchema]
+  }
+);
+
+mongoose.model("Compliment", ComplimentSchema);
+mongoose.model("Praise", PraiseSchema);
+if(process.env.NODE_ENV == "production"){
+  mongoose.connect(process.env.MONGOLAB_URL);
+}else{
+  mongoose.connect("mongodb://localhost/knopliments");
+}
+
+module.exports = mongoose;
