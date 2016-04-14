3dbf4ee05393114081caef45d434a734028d888b
Christine Movius <christine.movius@gmail.com>
2016-04-14 11:24:06 -0400

    Main app-welcome page successfully displays on localhost.

--- index.js
+++ index.js
@@ -2,3 +2,26 @@ var express = require("express");
 var parser = require("body-parser");
 var hbs = require("express-handlebars");
 var mongoose = require("./db/connection");
+
+var app = express();
+
+var Compliment = mongoose.model("Compliment");
+
+app.set("port", process.env.PORT || 3001);
+app.set("view engine", "hbs");
+app.engine(".hbs", hbs({
+  extname:        ".hbs",
+  partialsDir:    "views/",
+  layoutsDir:     "views/",
+  defaultLayout:  "layout-main"
+}));
+app.use("/assets", express.static("public"));
+app.use(parser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+  res.render("app-welcome");
+});
+
+app.listen(app.get("port"), function(){
+  console.log("It's aliiive!");
+});
