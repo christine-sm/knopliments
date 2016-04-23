5ad575e3b099c3d6e60544b0f2830fb27ece6075
Christine Movius <christine.movius@gmail.com>
2016-04-23 13:04:45 -0400

    Remove handlebars dependency and hbs files.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var express = require("express");
 var parser = require("body-parser");
-var hbs = require("express-handlebars");
 var mongoose = require("./db/connection");
 
 var app = express();
@@ -8,13 +7,6 @@ var app = express();
 var Compliment = mongoose.model("Compliment");
 
 app.set("port", process.env.PORT || 3001);
-// app.set("view engine", "hbs");
-// app.engine(".hbs", hbs({
-//   extname:        ".hbs",
-//   partialsDir:    "views/",
-//   layoutsDir:     "views/",
-//   defaultLayout:  "layout-main"
-// }));
 app.use("/assets", express.static("public"));
 app.use(parser.urlencoded({extended: true}));
 
